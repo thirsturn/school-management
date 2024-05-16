466d12dad285afd93059349c9b00daf3b332ac13
Thiranjaya Siriwardhana <kanujathiranjaya@gmail.com>
2024-05-16 09:52:11 +0530

    created files

---
+++ schoolManagement.c
@@ -0,0 +1,24 @@
+// include header files
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// function to add new students
+void enter_details()
+{
+}
+
+// functions to find students
+void find_student()
+{
+}
+
+// function to update details
+
+// function to delete data
+
+int main(int argc, char const *argv[])
+{
+
+    return 0;
+}
